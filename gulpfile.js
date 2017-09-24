4105b1679d3bff8855e374c978b9bbda04acd494
unknown <luhemeso@gmail.com>
2017-09-24 16:15:02 -0300

    mixin de criação de objeto

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    globally = require('gulp-sass-glob');
+
+gulp.task('sass', function () {
+    return gulp
+        .src('./framework/**/*.scss')
+        .pipe(globally())
+        .pipe(sass({
+            sourceComments: true,
+            errLogToConsole: true
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./framework/**/*.scss', ['sass']);
+});
